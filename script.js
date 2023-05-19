94fa0046658a00f9bcbbd924db1d3c556ce860f9
Josh Walthall <josh.walthall@gmail.com>
2023-05-19 16:16:09 -0400

    Create code outline in script.js; Create grid container in index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+// Initialize variable to hold grid size
+// Set grid container size
+// Get number of pixels per grid square by dividing grid container size by grid size
+// Create "grid size" number of rows, each containing "grid size" number of divs
+// Make grid squares change color when mouse passes over them
+// Create user entry pop-up button
+// When button is clicked, open pop-up to prompt for grid size
+// Remove existing grid, replace with grid of the user-provided size
